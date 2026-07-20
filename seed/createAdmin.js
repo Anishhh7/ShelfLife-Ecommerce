74f4182c0478c7a095a452c3f3393d8e8b8c0673
Anishh <pokharelanish099@gmail.com>
2026-07-20 23:57:33 +0545

    Feat- Admin Id created

---
+++ seed/createAdmin.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "./Config/config.env" });
+import User from "../Model/userModel.js";
+
+const User = require("./../models/userModel");
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB).then((con) => console.log("mongoose connected"));
+
+(async () => {
+ await User.create({
+  name: "Admin",
+  email: "admin@shelflife.com",
+  password: "passlogin123",
+  role: "admin"
+ });
+
+ console.log("SuperAdmin created");
+ process.exit();
+})();
