400c6d009808d2dc4748c7779b1cc307603ee86f
Anishh <pokharelanish099@gmail.com>
2026-07-21 21:23:34 +0545

    Feat- Add Filter and Search

---
+++ Utils/apiFeatures.js
@@ -0,0 +1,70 @@
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+    this.filterConditions = {};
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ["page", "sort", "limit", "fields", "search"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gt|gte|lte|lt)\b/g, (match) => `$${match}`);
+
+    const parsed = JSON.parse(queryStr);
+    this.filterConditions = { ...this.filterConditions, ...parsed };
+    this.query = this.query.find(parsed);
+    return this;
+  }
+
+  search() {
+    if (this.queryString.search) {
+      const searchCondition = {
+        $or: [
+          {
+            name: {
+              $regex: this.queryString.search,
+              $options: "i"
+            }
+          },
+          {
+            title: {
+              $regex: this.queryString.search,
+              $options: "i"
+            }
+          },
+          
+        ]
+      };
+      this.filterConditions = { ...this.filterConditions, ...searchCondition };
+      this.query = this.query.find(searchCondition);
+    }
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+    return this;
+  }
+  pagination() {
+    this.page = Math.max(1, Number(this.queryString.page) || 1);
+    this.limit = Math.min(
+      100,
+      Math.max(1, Number(this.queryString.limit) || 10)
+    );
+
+    const skip = (this.page - 1) * this.limit;
+
+    this.query = this.query.skip(skip).limit(this.limit);
+
+    return this;
+  }
+}
+export default APIFeatures;
