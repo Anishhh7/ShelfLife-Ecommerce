868f2a474b70b2463b2913baf5e909243c815df6
Anishh <pokharelanish099@gmail.com>
2026-08-02 21:48:41 +0545

    Add nested review routes to product router

--- Router/productRouter.js
+++ Router/productRouter.js
@@ -1,31 +1,33 @@
-import express from "express";
-import permission from "../Config/permission.js";
-import * as AuthController from "../Controller/authController.js";
-import * as ProductController from "../Controller/productController.js";
+import express from 'express';
+import permission from '../Config/permission.js';
+import * as AuthController from '../Controller/authController.js';
+import * as ProductController from '../Controller/productController.js';
+import reviewRouter from './reviewRouter.js';
 
 const router = express.Router();
 
-router.route("/").get(ProductController.getAllProduct);
-router.route("/:id").get(ProductController.getProductById);
+router.use('/:productId/reviews', reviewRouter);
+
+router.get('/', ProductController.getAllProduct);
+router.get('/:id', ProductController.getProductById);
 
 router.use(AuthController.protect);
 
-router
- .route("/")
- .post(
+router.post(
+  '/',
   AuthController.restrictTo(...permission.product.create),
   ProductController.createProduct
- );
+);
 
 router
- .route("/:id")
- .patch(
-  AuthController.restrictTo(...permission.product.create),
-  ProductController.updateProduct
- )
- .delete(
-  AuthController.restrictTo(...permission.product.create),
-  ProductController.deleteProduct
- );
+  .route('/:id')
+  .patch(
+    AuthController.restrictTo(...permission.product.create),
+    ProductController.updateProduct
+  )
+  .delete(
+    AuthController.restrictTo(...permission.product.create),
+    ProductController.deleteProduct
+  );
 
 export default router;
