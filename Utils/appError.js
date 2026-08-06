29291c6ac89810698e62423644bd521d51d50e92
Anishh <pokharelanish099@gmail.com>
2026-08-06 23:32:34 +0545

    Refactor AppError utility

--- Utils/appError.js
+++ Utils/appError.js
@@ -1,14 +1,17 @@
-class AppError extends Error{
-    constructor(message, statusCode) {
-        super(message);
+class AppError extends Error {
+  constructor(message, statusCode, errors = null) {
+    super(message);
 
-        this.statusCode = statusCode;
-        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4')
+      ? 'fail'
+      : 'error';
 
-        this.isOperational = true;
+    this.errors = errors;
+    this.isOperational = true;
 
-        Error.captureStackTrace(this, this.constructor);
-    }
+    Error.captureStackTrace(this, this.constructor);
+  }
 }
 
 export default AppError;
