996b3d979aec7ac770a55fa784b5a58988d7d5b9
Anishh <pokharelanish099@gmail.com>
2026-08-08 23:19:01 +0545

    Fix validation error handling

--- Utils/validate.js
+++ Utils/validate.js
@@ -1,8 +1,10 @@
+import AppError from './appError.js';
+
 export const validate = (schema) => (req, res, next) => {
   const result = schema.safeParse(req.body);
 
   if (!result.success) {
-    return next(result.error);
+    return next(new AppError(result.error.issues[0].message, 400));
   }
 
   req.body = result.data;
